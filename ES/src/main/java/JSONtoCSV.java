d9732d8e3ee1163af855979fb54548bb0135ac24
afonsoaguas <afonsofaguas@gmail.com>
2023-04-14 19:21:39 +0100

    jsonToCsv

--- ES/src/main/java/JSONtoCSV.java
+++ ES/src/main/java/JSONtoCSV.java
@@ -1,31 +1,43 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class JSONtoCSV {
 
     public static void main(String[] args) {
-        /*
-        ObjectMapper objectMapper = new ObjectMapper();
-        MappingIterator<Horario> dataIterator = objectMapper.readerFor(Horario.class)
-                .readValues(new File("data.json"));
-        List<Horario> dataList = dataIterator.readAll();
+        convertJSONtoCSV(new File("src/main/java/output.json"));
+    }
 
-        // convert objects to CSV
+    public static void convertJSONtoCSV(File jsonFile) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MappingIterator<Horario> dataIterator;
+        List<Horario> dataList;
+        try {
+            dataIterator = objectMapper.readerFor(Horario.class).readValues(jsonFile);
+            dataList = dataIterator.readAll();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         CsvMapper csvMapper = new CsvMapper();
         CsvSchema csvSchema = csvMapper.schemaFor(Horario.class).withHeader();
-        String csv = csvMapper.writer(csvSchema)
-                .writeValueAsString(dataList);
-
-        // write CSV to file
-        File csvFile = new File("data.csv");
-        FileUtils.writeStringToFile(csvFile, csv, "UTF-8");
-
-         */
+        String csv;
+        try {
+            csv = csvMapper.writer(csvSchema).writeValueAsString(dataList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        File csvFile = new File("output.csv");
+        try (PrintWriter writer = new PrintWriter(csvFile, StandardCharsets.UTF_8)) {
+            writer.print(csv);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
