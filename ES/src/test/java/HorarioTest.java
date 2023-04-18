ed2b2ec24e3c66bb6a14de36fb91c05de1e2f908
Feroconas <rafatinoni@gmail.com>
2023-04-18 19:48:30 +0100

    horarioTest class

---
+++ ES/src/test/java/HorarioTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HorarioTest {
+
+    @Test
+    void getAulas() {
+    }
+
+    @Test
+    void adicionarAula() {
+    }
+
+    @Test
+    void removerAula() {
+    }
+
+    @Test
+    void lerCSV() {
+    }
+
+    @Test
+    void lerJSON() {
+    }
+}
