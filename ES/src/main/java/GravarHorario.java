1e0c2a7680cac8dea760f91e1f509346f5bdd138
Leonardo Mota <lcpma@iscte-iul.pt>
2023-04-18 15:38:28 +0100

    Quase terminado o gravaEmCSV (ver comentarios)

--- ES/src/main/java/GravarHorario.java
+++ ES/src/main/java/GravarHorario.java
@@ -14,8 +14,15 @@ public class GravarHorario {
             CsvMapper csvMapper = new CsvMapper();
             CSVWriter writer = new CSVWriter(new FileWriter(csvFile), ';', CSVWriter.NO_QUOTE_CHARACTER,
                     CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
-            for (Aula a : h.getAulas()){
-                writer.writeNext({a.getCursos(),a.getUcs(),a.getTurno().getNome(),a.getTurmas(),a.getTurno().getNumInscritos()}));
+            for (Aula a : h.getAulas()) {
+                // TODO alterar os toStrings dos GETS
+                String[] rowData = { a.getCursos().toString(), a.getUcs().toString(), a.getTurno().getNome(),
+                        a.getTurmas(),
+                        Integer.toString(a.getTurno().getNumInscritos()), a.getData().getDayOfWeek().toString(),
+                        a.getHoraInicio().toString(), a.getHoraFim().toString(), a.getData().toString(),
+                        a.getSala().getNome(), Integer.toString(a.getSala().getLotacao()) };
+
+                writer.writeNext(rowData);
             }
         } catch (IOException e) {
             System.err.println("gravaEmCSV(h,caminhoDeOutput): Erro ao escrever no ficheiro");
--- ES/src/main/java/Sala.java
+++ ES/src/main/java/Sala.java
@@ -2,13 +2,21 @@ public class Sala {
     private String nome;
     private int lotacao;
 
-
-    public Sala(String nome, int lotacao){
+    public Sala(String nome, int lotacao) {
         this.lotacao = lotacao;
         this.nome = nome;
     }
+
     @Override
-    public String toString(){
-        return nome + " | lotação: "+lotacao ;
+    public String toString() {
+        return nome + " | lotação: " + lotacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getLotacao() {
+        return lotacao;
     }
 }
