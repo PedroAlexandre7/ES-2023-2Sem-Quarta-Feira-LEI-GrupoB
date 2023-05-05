dd4fbd8b8e5f096a4160f9e7a892d6db514a4c5c
afonsoaguas <afonsofaguas@gmail.com>
2023-05-05 13:58:31 +0100

    Ja funciona o site

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,20 +11,14 @@ public class Server extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Crie a agenda e adicione algumas aulas
         Horario horario = new Horario();
         try {
             horario.lerCSV(new File("input.csv"));
         } catch (Exception e) {
             System.out.println("Erro ao ler o ficheiro");
         }
-        //horario.adicionarAula(new Aula("Matemática", "João", "08:00", "10:00"));
-        //horario.adicionarAula(new Aula("História", "Maria", "10:00", "12:00"));
-
-        // Defina a agenda como um atributo da solicitação
         request.setAttribute("horario", horario);
 
-        // Encaminhe a solicitação para a página JSP da agenda
-        request.getRequestDispatcher("agenda.jsp").forward(request, response);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
