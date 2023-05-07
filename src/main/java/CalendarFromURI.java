90c97f7fe641b6b33f425916d87d40b5234f21e3
Feroconas <rafatinoni@gmail.com>
2023-05-07 19:45:03 +0100

    Correção no CalendarFromURI

--- src/main/java/CalendarFromURI.java
+++ src/main/java/CalendarFromURI.java
@@ -11,36 +11,39 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class CalendarFromURI {
 
     private CalendarFromURI() {
     }
 
-
     public static Calendar getCalendar(String uri) throws URISyntaxException, IOException, ParserException {
-            if (uri.startsWith("webcal"))
-                uri = uri.replaceFirst("webcal", "https");
-            URL url = new URI(uri).toURL();
-            System.setProperty("net.fortuna.ical4j.timezone.cache.impl", "net.fortuna.ical4j.util.MapTimeZoneCache");
-            CalendarBuilder calendarBuilder = new CalendarBuilder();
-            return calendarBuilder.build(url.openStream());
-        }
-
+        if (uri.startsWith("webcal"))
+            uri = uri.replaceFirst("webcal", "https");
+        URL url = new URI(uri).toURL();
+        System.setProperty("net.fortuna.ical4j.timezone.cache.impl", "net.fortuna.ical4j.util.MapTimeZoneCache");
+        CalendarBuilder calendarBuilder = new CalendarBuilder();
+        return calendarBuilder.build(url.openStream());
+    }
 
     public static Horario CalendarToHorario(Calendar calendar) {
         Horario horario = new Horario();
         for (CalendarComponent calendarComponent : calendar.getComponents()) {
             Object[] properties = calendarComponent.getProperties().toArray();
             String[] descriptionProperties = properties[5].toString().split("\\\\n");
-            String uc = descriptionProperties[1].replaceFirst("Unidade de execução: ", "");
-            Turno turno = new Turno(descriptionProperties[3].replaceFirst("Turno: ", ""), 0);
-            String[] dataHoraInicio = descriptionProperties[4].replaceFirst("Início: ", "").split(" ");
+            String uc = Pattern.compile("Unidade de execução: ", Pattern.CANON_EQ).matcher(descriptionProperties[1]).replaceFirst("");
+            Turno turno;
+            if (descriptionProperties[3].contains("Turno: "))
+                turno = new Turno(descriptionProperties[3].replaceFirst("Turno: ", ""), 0);
+            else
+                turno = new Turno(Pattern.compile("Descrição: ", Pattern.CANON_EQ).matcher(descriptionProperties[2]).replaceFirst(""), 0);
+            String[] dataHoraInicio = Pattern.compile("Início: ", Pattern.CANON_EQ).matcher(descriptionProperties[4]).replaceFirst("").split(" ");
             String[] dataHoraFim = descriptionProperties[5].replaceFirst("Fim: ", "").split(" ");
             LocalDate data = LocalDate.parse(dataHoraInicio[0]);
             LocalTime horaInicio = LocalTime.parse(dataHoraInicio[1]);
             LocalTime horaFim = LocalTime.parse(dataHoraFim[1]);
-            String salaNome = properties[6].toString().replaceFirst("LOCATION:", "").replaceAll("\\\\", "");
+            String salaNome = properties[6].toString().replaceFirst("LOCATION:", "").replaceAll("[\\\\\n\r]", "");
             String diaDaSemana = translateWeekDayEnum(data.getDayOfWeek());
             horario.adicionarAula(new Aula(new ArrayList<>(), uc, turno, new ArrayList<>(), diaDaSemana, horaInicio, horaFim, new Sala(salaNome, 0), data));
         }
